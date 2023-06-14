4cb010ad332d0354f5f8792b23ed5d1e975e6252
tgartland89 <thomas.gartland@gmail.com>
2023-06-14 16:03:56 -0600

    completed lab and added notes

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,58 @@
+
 def create_an_empty_list():
-    return None
+# add and array to clear pytest -1
+# Module list_crud.py contains a function "create_an_empty_list()" that creates and returns an empty list. . 
+    return []
 
 def create_a_list():
-    return None
+# add and array with 1,2,3,4 to clear pytest -2    
+# # Module list_crud.py contains a function "create_a_list()" that creates and returns a list of length 4.      
+    return [1,2,3,4]
 
 def add_element_to_end_of_list(l, element):
-    return None
+# # add l.append(element) and return l to clear pytest- 3           
+# Module list_crud.py contains a function "add_element_to_end_of_list" that adds an element to the end of a list. .  
+    l.append(element)
+    return l
 
 def add_element_to_start_of_list(l, element):
-    return None
+# # add l.instert(0,element) and return l to clear pytest 4-           
+# Module list_crud.py contains a function "add_element_to_start_of_list" that adds an element to the start of a list. .  
+    l.insert(0, element)
+    return l
 
 def remove_element_from_end_of_list(l):
-    return None
+# # add 1.pop() and return l to clear pytest -5          
+# Module list_crud.py contains a function "remove_element_from_start_of_list()" that removes an element from the start of a list. . 
+    l.pop()
+    return l
 
 def remove_element_from_start_of_list(l):
-    return None
+# add del l[0] and return l to clear pytest 6-           
+# Module list_crud.py contains a function "add_element_to_start_of_list" that adds an element to the start of a list. 
+    del l[0]
+    return l
 
 def retrieve_first_element_from_list(l):
-    return None
+# addded l[0] to reutrn to clear pytest-7
+# Module list_crud.py contains a function "retrieve_first_element_from_list()" that retrieves the first element from a list. .   
+    return l[0]
 
 def retrieve_element_from_index(l, index):
-    return None
+# add return l[index] to clear pytest 8
+# Module list_crud.py contains a function "retrieve_element_from_index()" that takes a list and an index as arguments and returns the list's element at that index. .    
+    return l[index]
 
 def retrieve_last_element_from_list(l):
-    return None
+# add l[-1] to return to clear pytest 9
+# Module list_crud.py contains a function "retrieve_last_element_from_list()" that retrieves the last element from a list. .  
+    return l[-1]
+
+                                                               
+                                                                  
+                                                        
+                                                    
+                                           
+                                         
+                                            
+    
